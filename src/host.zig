72250449ba19f90aafc43d9293030f2bff65392b
Majid Hatami <hatami6570@gmail.com>
2025-08-28 16:43:56 +0330

    refactor: make it work for most scenarios

--- src/host.zig
+++ src/host.zig
@@ -6,7 +6,7 @@ const print = std.debug.print;
 const SocketHandler = struct {
     socket_fd: std.posix.fd_t,
     page_controller: *PageController,
-    count: i32,
+    config: *Config,
 
     fn run(self: *SocketHandler) !void {
         while (true) {
@@ -27,8 +27,9 @@ const SocketHandler = struct {
                 print("Received message: {s}\n", .{msg});
 
                 if (std.mem.eql(u8, msg, "inc")) {
-                    self.count += 1;
-                    try self.page_controller.setCount(self.count);
+                    try self.config.inc();
+                    try self.config.writeCount();
+                    try self.page_controller.setCount(self.config.count);
                 }
             }
         }
@@ -39,7 +40,7 @@ const PageController = struct {
     allocator: std.mem.Allocator,
     win: *webui,
 
-    fn setCount(self: *PageController, count: i32) !void {
+    fn setCount(self: *PageController, count: u32) !void {
         const set_count_js = try std.fmt.allocPrintZ(self.allocator, "SetCount({d});", .{count});
         defer self.allocator.free(set_count_js);
 
@@ -55,24 +56,133 @@ const PageController = struct {
 };
 
 const Config = struct {
-    title: []u8,
-    counter: i32,
+    const filename = "config.txt";
+    const count_len = 4;
+
+    allocator: std.mem.Allocator,
+    file: ?std.fs.File,
+    title: ?[]u8,
+    count: u32,
+    count_str: [5]u8,
+    count_position_in_file: u64,
+
+    fn init(allocator: std.mem.Allocator) !Config {
+        var config = Config{
+            .allocator = allocator,
+            .file = null,
+            .title = null,
+            .count = 0,
+            .count_str = [_]u8{ ' ', '0', '0', '0', '0' },
+            .count_position_in_file = 0,
+        };
+
+        try config.ensureExists();
+        try config.readConfig();
+
+        return config;
+    }
+
+    fn deinit(self: *Config) void {
+        if (self.file) |file| {
+            file.close();
+        }
+        if (self.title) |title| {
+            self.allocator.free(title);
+        }
+    }
+
+    fn ensureExists(self: *Config) !void {
+        const cwd = std.fs.cwd();
+        const file = cwd.openFile(filename, .{ .mode = .read_write }) catch |err| switch (err) {
+            error.FileNotFound => {
+                try self.writeDefault();
+                return;
+            },
+            else => return err,
+        };
+
+        const file_info = try file.stat();
+        if (file_info.size == 0) {
+            file.close();
+            try self.writeDefault();
+            return;
+        }
+
+        self.file = file;
+    }
+
+    fn writeDefault(self: *Config) !void {
+        var file = try std.fs.cwd().createFile(filename, .{ .read = true });
+
+        try file.writeAll("Title: Your First Counter\nCount: 0000");
+
+        self.file = file;
+    }
+
+    fn inc(self: *Config) !void {
+        self.count += 1;
+        _ = try std.fmt.bufPrint(&self.count_str, " {:0>4}", .{self.count});
+        print("new count: {d}, {s}\n", .{ self.count, self.count_str });
+    }
+
+    fn writeCount(self: *Config) !void {
+        if (self.file) |file| {
+            try file.seekTo(self.count_position_in_file);
+            try file.writeAll(self.count_str[0..]);
+        }
+    }
+
+    fn readConfig(self: *Config) !void {
+        var buf: [1024]u8 = undefined;
+
+        if (self.file) |file| {
+            try file.seekTo(0);
+
+            const bytes_read = try file.readAll(&buf);
+            const config_text = buf[0..bytes_read];
+
+            const title_label = "Title:";
+            const title_start = std.mem.indexOf(u8, config_text, title_label) orelse return error.DecodeError;
+            const title_value_start = title_start + title_label.len;
+
+            const title_line_end = std.mem.indexOf(u8, config_text[title_value_start..], "\n") orelse return error.DecodeError;
+            const title_value_end = title_value_start + title_line_end;
+
+            const title_value = config_text[title_value_start..title_value_end];
+            const trimmed_title_value = std.mem.trim(u8, title_value, " \r\n\t");
+
+            if (self.title) |title| {
+                self.allocator.free(title);
+                self.title = null;
+            }
+
+            self.title = try self.allocator.alloc(u8, trimmed_title_value.len);
+            std.mem.copyForwards(u8, self.title.?, trimmed_title_value);
+
+            const count_label = "Count:";
+            const count_start = std.mem.indexOf(u8, config_text[title_value_end..], count_label) orelse return error.DecodeError;
+            const count_value_start = title_value_end + count_start + count_label.len;
+
+            const count_line_end = std.mem.indexOf(u8, config_text[count_value_start..], "\n") orelse (config_text.len - count_value_start);
+            const count_value_end = count_value_start + count_line_end;
+
+            const count_value = config_text[count_value_start..count_value_end];
+            const trimmed_count_value = std.mem.trim(u8, count_value, " \r\n\r");
+
+            self.count = try std.fmt.parseInt(u32, trimmed_count_value, 10);
+            self.count_position_in_file = count_value_start;
+        }
+    }
 };
 
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-    var config = Config{ .title = &[_]u8{}, .counter = 0 };
-    const filename = "config.txt";
+    var config = try Config.init(allocator);
+    defer config.deinit();
 
-    var file = try ensureConfigExists(filename);
-    defer file.close();
-
-    try readConfig(allocator, &file, &config);
-    defer allocator.free(config.title);
-
-    print("Config => Title: {s}, Counter: {d}\n", .{ config.title, config.counter });
+    print("Config => Title: {s}, Count: {d}\n", .{ config.title orelse "", config.count });
 
     const socket_path = "/tmp/counterz.sock";
     _ = std.fs.deleteFileAbsolute(socket_path) catch |err| switch (err) {
@@ -100,13 +210,13 @@ pub fn main() !void {
         .win = &win,
     };
 
-    try page_controller.setTitle(config.title);
-    try page_controller.setCount(config.counter);
+    try page_controller.setTitle(config.title orelse "");
+    try page_controller.setCount(config.count);
 
     var socket_handler = SocketHandler{
         .socket_fd = sock_fd,
         .page_controller = &page_controller,
-        .count = config.counter,
+        .config = &config,
     };
 
     const thread = try std.Thread.spawn(.{}, SocketHandler.run, .{&socket_handler});
@@ -114,69 +224,4 @@ pub fn main() !void {
 
     webui.wait();
     webui.clean();
-}
-
-fn ensureConfigExists(filename: []const u8) !std.fs.File {
-    const cwd = std.fs.cwd();
-    const file = cwd.openFile(filename, .{ .mode = .read_write }) catch |err| switch (err) {
-        error.FileNotFound => {
-            return try writeDefaultConfig(filename);
-        },
-        else => return err,
-    };
-
-    const file_info = try file.stat();
-    if (file_info.size == 0) {
-        file.close();
-        return try writeDefaultConfig(filename);
-    }
-
-    return file;
-}
-
-fn writeDefaultConfig(filename: []const u8) !std.fs.File {
-    var file = try std.fs.cwd().createFile(filename, .{});
-
-    try file.writeAll("Title: \nCounter: 0000");
-
-    return file;
-}
-
-fn readConfig(allocator: std.mem.Allocator, file: *std.fs.File, config: *Config) !void {
-    var buf: [1024]u8 = undefined;
-
-    try file.seekTo(0);
-
-    const bytes_read = try file.readAll(&buf);
-    const config_text = buf[0..bytes_read];
-
-    const title_label = "Title:";
-    const title_start = std.mem.indexOf(u8, config_text, title_label) orelse return error.DecodeError;
-    const title_value_start = title_start + title_label.len;
-
-    const title_line_end = std.mem.indexOf(u8, config_text[title_value_start..], "\n") orelse return error.DecodeError;
-    const title_value_end = title_value_start + title_line_end;
-
-    const title_value = config_text[title_value_start..title_value_end];
-    const trimmed_title_value = std.mem.trim(u8, title_value, " \r\n\t");
-
-    if (config.title.len != 0) {
-        allocator.free(config.title);
-        config.title = &[_]u8{};
-    }
-
-    config.title = try allocator.alloc(u8, trimmed_title_value.len);
-    std.mem.copyForwards(u8, config.title, trimmed_title_value);
-
-    const counter_label = "Counter:";
-    const counter_start = std.mem.indexOf(u8, config_text[title_value_end..], counter_label) orelse return error.DecodeError;
-    const counter_value_start = title_value_end + counter_start + counter_label.len;
-
-    const counter_line_end = std.mem.indexOf(u8, config_text[counter_value_start..], "\n") orelse (config_text.len - counter_value_start);
-    const counter_value_end = counter_value_start + counter_line_end;
-
-    const counter_value = config_text[counter_value_start..counter_value_end];
-    const trimmed_counter_value = std.mem.trim(u8, counter_value, " \r\n\r");
-
-    config.counter = try std.fmt.parseInt(i32, trimmed_counter_value, 10);
 }
