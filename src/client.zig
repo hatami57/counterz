50d15d5ae423f1edd1247d6ca0b6874ddbf55047
Majid Hatami <hatami6570@gmail.com>
2025-08-28 16:43:15 +0330

    fix: update socket address

--- src/client.zig
+++ src/client.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    const socket_path = "/tmp/zig_counter.sock";
+    const socket_path = "/tmp/counterz.sock";
     const sock_fd = try std.posix.socket(std.posix.AF.UNIX, std.posix.SOCK.STREAM, 0);
     defer std.posix.close(sock_fd);
 
